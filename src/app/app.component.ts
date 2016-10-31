d0c808cec8214d9246746e44a6e66727ed150092
JT-Bruch <jt.bruch@crownpeak.com>
2016-10-31 14:13:34 -0600

    NS: Get the thing to be broken.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  testButton() {
+    console.log("firebase.database.ServerValue.TIMESTAMP:" + firebase.database.ServerValue.TIMESTAMP);
+  }
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,3 +2,6 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
+declare namespace firebase.database.ServerValue {
+  var TIMESTAMP: any
+}
